bc538eaf9e7a4e32d09d581c3e847360906c6ed5
zhangyi1237654 <329757823@qq.com>
2017-06-19 18:25:48 +0800

    struts2
    test

---
+++ src/com/zy/action/LoginAction.java
@@ -0,0 +1,40 @@
+package com.zy.action;
+
+import java.util.Map;
+
+import org.apache.struts2.interceptor.SessionAware;
+
+import com.opensymphony.xwork2.ActionSupport;
+import com.opensymphony.xwork2.ModelDriven;
+import com.zy.entity.User;
+
+@SuppressWarnings("serial")
+public class LoginAction extends ActionSupport implements ModelDriven<User>,SessionAware {
+
+	private User user = new User();
+	
+	private Map<String, Object> session ;
+
+	@Override
+	public User getModel() {
+		return user;
+	}
+
+	@Override
+	public void setSession(Map<String, Object> session) {
+		this.session = session;
+	}
+	
+	public String login(){
+		if("admin".equals(user.getUsername())
+				&& "123".equals(user.getPassword())){
+			session.put("loginInfo", user.getUsername());
+			return SUCCESS;
+		}else{
+			session.put("loginError", "用户名或密码不正确!");
+			return ERROR;
+		}
+	}
+	
+	
+}
